abe5700719ccbdf0a6d9c64bb8be3400d49ece24
Junior Oliva <jrfoliva@gmail.com>
2023-10-02 23:31:38 -0300

    Add project files.

---
+++ ExemploSemInterface/Services/BrazilTaxService.cs
@@ -0,0 +1,19 @@
+﻿using ExemploComInterface.Services;
+
+namespace ExemploSemInterface.Services
+{
+    internal class BrazilTaxService : ITaxService
+    {
+        public double Tax(double amount)
+        {
+            if (amount <= 100.0)
+            {
+                return amount * 0.20;
+            }
+            else
+            {
+                return amount * 0.15;
+            }
+        }
+    }
+}
---
+++ ExemploSemInterface/Services/ITaxService.cs
@@ -0,0 +1,7 @@
+﻿namespace ExemploComInterface.Services
+{
+    internal interface ITaxService
+    {
+        double Tax(double amount);
+    }
+}
